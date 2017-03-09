2abad3fe2109ede67e1628fdfa4ae47f8cd8e9ac
minashaigan <mina.shaigan@gmail.com>
2017-03-09 11:25:55 +0330

    course part

---
+++ database/migrations/2017_03_09_074808_create_coursepart_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursepartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coursepart', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('start');
+            $table->string('end');
+            $table->string('day')->nullable();
+            $table->integer('duration')->default('0');
+            $table->integer('course_id')->unsigned()->index();
+            $table->timestamps();
+            /*
+             * If you soft delete
+             * something it is still there so it's able to be
+             * recovered (un-deleted) Hard delete,
+             * or permanent delete, deletes it
+             * off your database for good.
+             * So you would not be able
+             * to get that post back !
+             * unless you had a backup
+             */
+            $table->softDeletes();
+            $table->index(['deleted_at']);
+        });
+        Schema::table('sections', function($table) {
+            $table->foreign('course_id')
+                ->references('id')
+                ->on('courses')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coursepart');
+    }
+}
