8ce58387704b6073507123d7948921cdfb823b24
hossein <h.faghihi15@gmail.com>
2017-03-11 23:23:25 +0330

    database seeder set

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(CoursesTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
+        $this->call(TeachersTableSeeder::class);
+        $this->call(UsecourseTableSeeder::class);
+        $this->call(SectionsTableSeeder::class);
+        $this->call(PackagesTableSeeder::class);
+
+        $this->call(TakeCourseTableSeeder::class);
+        $this->call(TeacherTagTableSeeder::class);
+        $this->call(ReviewCourseTableSeeder::class);
+        $this->call(ReviewTeacherTableSeeder::class);
+        $this->call(PackageCourseTableSeeder::class);
+        $this->call(CourseTeacherTableSeeder::class);
+        $this->call(CourseTagTableSeeder::class);
     }
 }
