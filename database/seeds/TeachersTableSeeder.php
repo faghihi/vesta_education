2b6bad1c7958230f92c604475f1d9cf321d1fc73
minashaigan <mina.shaigan@gmail.com>
2017-03-17 19:52:46 +0330

    change seed

--- database/seeds/TeachersTableSeeder.php
+++ database/seeds/TeachersTableSeeder.php
@@ -11,15 +11,10 @@ class TeachersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('teachers')->insert(['name' => 'faghihi','occupation'=>'hellohellohello','introduction'=>'hellohellohellohello']);
-        DB::table('teachers')->insert(['name' => 'me','occupation'=>'hellohellohello','introduction'=>'hellohellohellohellohello']);
-        DB::table('teachers')->insert(['name' => 'me','occupation'=>'hellohellohello','introduction'=>'hellohellohellohellohello']);
-        DB::table('teachers')->insert(['name' => 'me','occupation'=>'hellohellohello','introduction'=>'hello']);
-        DB::table('teachers')->insert(['name' => 'me','occupation'=>'hellohellohello','introduction'=>'hellohellohellohellohellohello']);
-        DB::table('teachers')->insert(['name' => 'me','occupation'=>'hellohellohello','introduction'=>'hellohellohellohello']);
-        DB::table('teachers')->insert(['name' => 'me','occupation'=>'hellohellohello','introduction'=>'hellohellohellohellohellohello']);
-        DB::table('teachers')->insert(['name' => 'me','occupation'=>'hellohellohello','introduction'=>'hellohellohello']);
-        DB::table('teachers')->insert(['name' => 'me','occupation'=>'hellohellohello','introduction'=>'hellohellohellohello']);
-        DB::table('teachers')->insert(['name' => 'me','occupation'=>'hellohellohello','introduction'=>'hellohellohellohellohello']);
+        DB::table('teachers')->insert(['name' => 'faghihi','occupation'=>'web developer','introduction'=>'back end']);
+        DB::table('teachers')->insert(['name' => 'mirzaie','occupation'=>'web designer','introduction'=>'front end']);
+        DB::table('teachers')->insert(['name' => 'shaigan','occupation'=>'web developer','introduction'=>'back end']);
+        DB::table('teachers')->insert(['name' => 'me','occupation'=>'hello','introduction'=>'hello']);
+
     }
 }
