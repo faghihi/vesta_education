855ea00088e7f90923f984e902a1e6804c1b72c2
hossein <h.faghihi15@gmail.com>
2017-03-17 20:54:17 +0330

    course grid added index coloring fixed

---
+++ resources/views/courses/courses-list.blade.php
@@ -0,0 +1,269 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>UniLearn - Education and Courses Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <!-- style -->
+    <link rel="shortcut icon" href="/img/favicon.png">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/font-awesome.css">
+    <!--<link rel="stylesheet" type="text/css" href="tuner/css/colorpicker.css" />-->
+    <!--<link rel="stylesheet" type="text/css" href="tuner/css/styles.css" />-->
+    <link rel="stylesheet" type="text/css" href="/css/jquery.fancybox.css" />
+    <link rel="stylesheet" href="/css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="/rs-plugin/css/settings.css" media="screen">
+    <link rel="stylesheet" href="/css/Kimia.css">
+    <!--styles -->
+</head>
+<body class="shop">
+<!-- header -->
+<!-- page header -->
+<header>
+    <!-- menu -->
+    @include('header')
+    <!-- / menu -->
+    <!-- page title -->
+    <div class="page-title">
+        <div class="grid-row">
+            <h1>دوره ها</h1>
+            <nav class="bread-crumb">
+                <a href="index.html">خانه</a>
+                <i class="fa fa-long-arrow-left"></i>
+                <a href="courses-grid.html">دوره ها </a>
+            </nav>
+        </div>
+    </div>
+    <!-- / page title -->
+</header>
+<!-- / header -->
+<!-- page content -->
+<div class="page-content">
+    <div class="container">
+        <!-- main content -->
+        <main>
+            <section>
+                <div class="clear-fix">
+                    <div class="grid-col-row">
+                        <div class="grid-col grid-col-4">
+                            <!-- course item -->
+                            <div class="course-item">
+                                <div class="course-hover">
+                                    <img src="/pic/370x280-img-1.jpg" data-at2x="/pic/370x280-img-1@2x.jpg" alt>
+                                    <div class="hover-bg bg-color-1"></div>
+                                    <a href="#">Learn More</a>
+                                </div>
+                                <div class="course-name clear-fix">
+                                    <h3><a href="#">کارگاه آموزشی هدفگذاری</a></h3>
+                                    <span class="price">۶۵۰,۰۰۰ تومان</span>
+                                </div>
+                                <div class="course-date bg-color-1 clear-fix">
+                                    <div class="day"><i class="fa fa-calendar"></i>شروع از ۱۶ اسفند</div><div class="time"><i class="fa fa-clock-o"></i>ساعت ۱۵:۰۰</div>
+                                    <div class="divider"></div>
+                                    <div class="description">
+                                        <p>
+                                            مهمترین عامل ، در کسب هر موفقیتی ، تدوین و مشخص کردن دقیق اهداف می باشد. هدفگذاری مهمترین عامل حرکت و خلق آینده است ، هدفها هستند که مقصد و مسیر زندگی فردی و سازمانی را روشن می کنند.هدف گذاری به مثابه ترسیم جاده ای برای آینده است .
+                                            مهمترین عامل ، در کسب هر موفقیتی ، تدوین و مشخص کردن دقیق اهداف می باشد. هدفگذاری مهمترین عامل حرکت و خلق آینده است ، هدفها هستند که مقصد و مسیر زندگی فردی و سازمانی را روشن می کنند.هدف گذاری به مثابه ترسیم جاده ای برای آینده است .
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- / course item -->
+                            <!-- course item -->
+                            <div class="course-item">
+                                <div class="course-hover">
+                                    <img src="/pic/370x280-img-4.jpg" data-at2x="/pic/370x280-img-4@2x.jpg" alt>
+                                    <div class="hover-bg bg-color-5"></div>
+                                    <a href="#">Learn More</a>
+                                </div>
+                                <div class="course-name clear-fix">
+                                    <span class="price">$85</span>
+                                    <h3><a href="#">Architecture Courses</a></h3>
+                                </div>
+                                <div class="course-date bg-color-5 clear-fix">
+                                    <div class="day"><i class="fa fa-calendar"></i>23 January</div><div class="time"><i class="fa fa-clock-o"></i>At 2:00 pm</div>
+                                    <div class="divider"></div>
+                                    <div class="description">Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis</div>
+                                </div>
+                            </div>
+                            <!-- / course item -->
+                        </div>
+                        <div class="grid-col grid-col-4">
+                            <!-- course item -->
+                            <div class="course-item">
+                                <div class="course-hover">
+                                    <img src="/pic/370x280-img-2.jpg" data-at2x="/pic/370x280-img-2@2x.jpg" alt>
+                                    <div class="hover-bg bg-color-2"></div>
+                                    <a href="#">Learn More</a>
+                                </div>
+                                <div class="course-name clear-fix">
+                                    <span class="price">Free</span>
+                                    <h3><a href="#">Campus Party</a></h3>
+                                </div>
+                                <div class="course-date bg-color-2 clear-fix">
+                                    <div class="day"><i class="fa fa-calendar"></i>12 January</div><div class="time"><i class="fa fa-clock-o"></i>At 4:00 pm</div>
+                                    <div class="divider"></div>
+                                    <div class="description">Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis</div>
+                                </div>
+                            </div>
+                            <!-- / course item -->
+                            <!-- course item -->
+                            <div class="course-item">
+                                <div class="course-hover">
+                                    <img src="/pic/370x280-img-5.jpg" data-at2x="/pic/370x280-img-5@2x.jpg" alt>
+                                    <div class="hover-bg bg-color-4"></div>
+                                    <a href="#">Learn More</a>
+                                </div>
+                                <div class="course-name clear-fix">
+                                    <span class="price">Free</span>
+                                    <h3><a href="#">Courses of Medical Care</a></h3>
+                                </div>
+                                <div class="course-date bg-color-4 clear-fix">
+                                    <div class="day"><i class="fa fa-calendar"></i>24 January</div><div class="time"><i class="fa fa-clock-o"></i>At 1:00 pm</div>
+                                    <div class="divider"></div>
+                                    <div class="description">Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis</div>
+                                </div>
+                            </div>
+                            <!-- / course item -->
+                        </div>
+                        <div class="grid-col grid-col-4">
+                            <!-- course item -->
+                            <div class="course-item">
+                                <div class="course-hover">
+                                    <img src="/pic/370x280-img-3.jpg" data-at2x="/pic/370x280-img-3@2x.jpg" alt>
+                                    <div class="hover-bg bg-color-3"></div>
+                                    <a href="#">Learn More</a>
+                                </div>
+                                <div class="course-name clear-fix">
+                                    <span class="price">$45</span>
+                                    <h3><a href="#">Design Practice</a></h3>
+                                </div>
+                                <div class="course-date bg-color-3 clear-fix">
+                                    <div class="day"><i class="fa fa-calendar"></i>22 January</div><div class="time"><i class="fa fa-clock-o"></i>At 6:30 pm</div>
+                                    <div class="divider"></div>
+                                    <div class="description">Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis</div>
+                                </div>
+                            </div>
+                            <!-- / course item -->
+                            <!-- course item -->
+                            <div class="course-item">
+                                <div class="course-hover">
+                                    <img src="/pic/370x280-img-6.jpg" data-at2x="/pic/370x280-img-6@2x.jpg" alt>
+                                    <div class="hover-bg bg-color-6"></div>
+                                    <a href="#">Learn More</a>
+                                </div>
+                                <div class="course-name clear-fix">
+                                    <span class="price">$105</span>
+                                    <h3><a href="#">Basic Cooking techniques</a></h3>
+                                </div>
+                                <div class="course-date bg-color-6 clear-fix">
+                                    <div class="day"><i class="fa fa-calendar"></i>26 January</div><div class="time"><i class="fa fa-clock-o"></i>At 3:30 pm</div>
+                                    <div class="divider"></div>
+                                    <div class="description">Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis</div>
+                                </div>
+                            </div>
+                            <!-- / course item -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <hr class="divider-color" />
+            <section>
+                <h2>Popular In Category</h2>
+                <div class="clear-fix">
+                    <div class="grid-col-row">
+                        <div class="grid-col grid-col-4">
+                            <!-- course item -->
+                            <div class="course-item">
+                                <div class="course-hover">
+                                    <img src="/pic/370x280-img-7.jpg" data-at2x="/pic/370x280-img-7@2x.jpg" alt>
+                                    <div class="hover-bg bg-color-1"></div>
+                                    <a href="#">Learn More</a>
+                                </div>
+                                <div class="course-name clear-fix">
+                                    <span class="price">$75</span>
+                                    <h3><a href="#">Science In The New Era</a></h3>
+                                </div>
+                                <div class="course-date bg-color-1 clear-fix">
+                                    <div class="day"><i class="fa fa-calendar"></i>11 January</div><div class="time"><i class="fa fa-clock-o"></i>At 3:00 pm</div>
+                                    <div class="divider"></div>
+                                    <div class="description">Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis</div>
+                                </div>
+                            </div>
+                            <!-- / course item -->
+                        </div>
+                        <div class="grid-col grid-col-4">
+                            <!-- course item -->
+                            <div class="course-item">
+                                <div class="course-hover">
+                                    <img src="/pic/370x280-img-8.jpg" data-at2x="/pic/370x280-img-8@2x.jpg" alt>
+                                    <div class="hover-bg bg-color-2"></div>
+                                    <a href="#">Learn More</a>
+                                </div>
+                                <div class="course-name clear-fix">
+                                    <span class="price">Free</span>
+                                    <h3><a href="#">Campus Party</a></h3>
+                                </div>
+                                <div class="course-date bg-color-2 clear-fix">
+                                    <div class="day"><i class="fa fa-calendar"></i>12 January</div><div class="time"><i class="fa fa-clock-o"></i>At 4:00 pm</div>
+                                    <div class="divider"></div>
+                                    <div class="description">Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis</div>
+                                </div>
+                            </div>
+                            <!-- / course item -->
+                        </div>
+                        <div class="grid-col grid-col-4">
+                            <!-- course item -->
+                            <div class="course-item">
+                                <div class="course-hover">
+                                    <img src="/pic/370x280-img-9.jpg" data-at2x="/pic/370x280-img-9@2x.jpg" alt>
+                                    <div class="hover-bg bg-color-3"></div>
+                                    <a href="#">Learn More</a>
+                                </div>
+                                <div class="course-name clear-fix">
+                                    <span class="price">$45</span>
+                                    <h3><a href="#">Design Practice</a></h3>
+                                </div>
+                                <div class="course-date bg-color-3 clear-fix">
+                                    <div class="day"><i class="fa fa-calendar"></i>22 January</div><div class="time"><i class="fa fa-clock-o"></i>At 6:30 pm</div>
+                                    <div class="divider"></div>
+                                    <div class="description">Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis</div>
+                                </div>
+                            </div>
+                            <!-- / course item -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <!-- / main content -->
+    </div>
+</div>
+<!-- / page content -->
+<!-- footer -->
+@include('footer')
+<!-- / footer -->
+<script src="/js/jquery.min.js"></script>
+<script src="/js/jquery.dotdotdot.min.js"></script>
+<script type='text/javascript' src='/js/jquery.validate.min.js'></script>
+<script src="/js/jquery.form.min.js"></script>
+<script src="/js/TweenMax.min.js"></script>
+<script src="/js/main.js"></script>
+<!-- jQuery REVOLUTION Slider  -->
+<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script src="/js/jquery.isotope.min.js"></script>
+
+<script src="/js/owl.carousel.min.js"></script>
+<script src="/js/jquery-ui.min.js"></script>
+<script src="/js/jflickrfeed.min.js"></script>
+<script src="/js/jquery.tweet.js"></script>
+<!--<script type='text/javascript' src='tuner/js/colorpicker.js'></script>-->
+<!--<script type='text/javascript' src='tuner/js/scripts.js'></script>-->
+<script src="/js/jquery.fancybox.pack.js"></script>
+<script src="/js/jquery.fancybox-media.js"></script>
+<script src="/js/retina.min.js"></script>
+<script src="/js/Kimia.js"></script>
+</body>
+</html>
