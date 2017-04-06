0ce08c8e39a858002527040f2eea352683317863
minashaigan <mina.shaigan@gmail.com>
2017-04-06 09:51:40 +0430

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 {{--<div class="panel-heading">Dashboard</div>--}}
 
                 <div class="panel-body center">
-                    شما وارد شده اید!
+                    !شما وارد شده اید
                 </div>
             </div>
         </div>
