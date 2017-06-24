0d8ac11b047b93a2520b96d2074a66eeba40961d
hossein <h.faghihi15@gmail.com>
2017-06-24 16:12:28 +0430

    about us and persian numbers added

--- public/js/main.js
+++ public/js/main.js
@@ -953,6 +953,23 @@ $(document).ready(function() {
       }
     });
   }
+  function walkNode(node) {
+    if (node.nodeType == 3) {
+      // Do your replacement here
+      node.data = node.data.replace(/\d/g,convert);
+    }
+
+    // Also replace text in child nodes
+    for (var i = 0; i < node.childNodes.length; i++) {
+      walkNode(node.childNodes[i]);
+    }
+  }
+
+  walkNode(document.getElementsByTagName('body')[0]);
+
+  function convert(a){
+    return ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'][a];
+  }
 
   /**/
   /* revolution slider */
---
+++ resources/views/page-about-us.blade.php
@@ -0,0 +1,395 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>UniLearn - Education and Courses Template</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+	<!-- style -->
+	<link rel="shortcut icon" href="img/favicon.png">
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/select2.css">
+	<link rel="stylesheet" href="css/main.css">
+	<!--<link rel="stylesheet" type="text/css" href="tuner/css/colorpicker.css" />-->
+	<!--<link rel="stylesheet" type="text/css" href="tuner/css/styles.css" />-->
+	<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css" />
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/Kimia.css">
+	<!--styles -->
+</head>
+<body class="">
+<header>
+	<!-- main menu -->
+@include('header')
+<!-- / main menu -->
+	<!-- page title -->
+	<div class="page-title">
+		<div class="grid-row">
+			<h1>درباره ی ما</h1>
+			<!-- bread crumb -->
+			<nav class="bread-crumb">
+				<a href="/">خانه</a>
+				<i class="fa fa-long-arrow-left"></i>
+				<a href="/about">درباره ی ما</a>
+			</nav>
+			<!-- / bread crumb -->
+		</div>
+	</div>
+	<!-- / page title -->
+</header>
+
+	<div class="page-content">
+		<main>
+			<section>
+				<div class="grid-row clear-fix">
+					<div class="grid-col-row">
+						<div class="grid-col grid-col-6">
+							<h2>Why We Are Better</h2>
+							<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis justo at suscipit. Vivamus orci urna, ornare vitae tellus in.</p>
+							<!-- accordions -->
+							<div class="accordions">
+								<!-- content-title -->
+								<div class="content-title active">Donec sollicitudin lacus?</div>
+								<!--/content-title -->
+								<!-- accordions content -->
+								<div class="content">Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. </div>
+								<!--/accordions content -->
+								<!-- content-title -->
+								<div class="content-title">Lorem ipsum dolor sit amet?</div>
+								<!--/content-title -->
+								<!-- accordions content -->
+								<div class="content">Nullam elementum tristique risus nec pellentesque. Pellentesque bibendum nunc eget nunc hendrerit auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur gravida urna nisl</div>
+								<!--/accordions content -->
+								<!-- content-title -->
+								<div class="content-title">Aenean commodo ligula eget dolor?</div>
+								<!--/content-title -->
+								<!-- accordions content -->
+								<div class="content">Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. </div>
+								<!--/accordions content -->
+								<!-- content-title -->
+								<div class="content-title">Moreno gotro ja pisit amet?</div>
+								<!--/content-title -->
+								<!-- accordions content -->
+								<div class="content">Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. </div>
+								<!--/accordions content -->
+							</div>
+							<!--/accordions -->
+							<a href="#" class="cws-button bt-color-3 border-radius alt icon-right page-about-us-view-detail">View Detail<i class="fa fa-angle-left"></i></a>
+						</div>
+						<div class="grid-col grid-col-6">
+							<div class="owl-carousel full-width-slider">
+								<div class="gallery-item picture">
+									<img src="pic/570x380-img-2.jpg" data-at2x="pic/570x380-img-2@2x.jpg" alt>
+								</div>
+								<div class="gallery-item picture">
+									<img src="pic/570x380-img-1.jpg" data-at2x="pic/570x380-img-1@2x.jpg" alt>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<hr class="divider-color" />
+			<section class="fullwidth-background">
+				<div class="grid-row">
+					<!-- carousel people -->
+					<div class="carousel-container">
+						<div class="title-carousel">
+							<h2>اساتید درس طراحی و توسعه اپلیکیشن</h2>
+							<div class="carousel-nav">
+								<div class="carousel-button">
+									<div class="prev"><i class="fa fa-angle-left"></i></div><!--
+								 --><div class="next"><i class="fa fa-angle-right"></i></div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-col-row left-margin-none">
+							<div class="owl-carousel owl-two-item">
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-1">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-1.jpg" data-at2x="pic/210x220-img-1@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>Jenny Doe</h3>
+											<span class="instructor-profession">Professor of Methematic</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!-- 
+												 --><a href="#" class="fa fa-facebook"></a><!-- 
+												 --><a href="#" class="fa fa-google-plus"></a><!-- 
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-2">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-2.jpg" data-at2x="pic/210x220-img-2@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>John Doe</h3>
+											<span class="instructor-profession">Lecturer of Design</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!-- 
+												 --><a href="#" class="fa fa-facebook"></a><!-- 
+												 --><a href="#" class="fa fa-google-plus"></a><!-- 
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-3">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-4.jpg" data-at2x="pic/210x220-img-4@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>Jessy Doe</h3>
+											<span class="instructor-profession">Professor of Methematic</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!-- 
+												 --><a href="#" class="fa fa-facebook"></a><!-- 
+												 --><a href="#" class="fa fa-google-plus"></a><!-- 
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-6">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-5.jpg" data-at2x="pic/210x220-img-5@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>Jenny Doe</h3>
+											<span class="instructor-profession">Professor of Methematic</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!-- 
+												 --><a href="#" class="fa fa-facebook"></a><!-- 
+												 --><a href="#" class="fa fa-google-plus"></a><!-- 
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- carousel people -->
+					<br>
+					<br>
+					<!-- carousel people -->
+					<div class="carousel-container">
+						<div class="title-carousel">
+							<h2>اساتید درس طراحی وب سایت</h2>
+							<div class="carousel-nav">
+								<div class="carousel-button">
+									<div class="prev"><i class="fa fa-angle-left"></i></div><!--
+								 --><div class="next"><i class="fa fa-angle-right"></i></div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-col-row left-margin-none">
+							<div class="owl-carousel owl-two-item">
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-3">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-4.jpg" data-at2x="pic/210x220-img-4@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>Jessy Doe</h3>
+											<span class="instructor-profession">Professor of Methematic</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!--
+												 --><a href="#" class="fa fa-facebook"></a><!--
+												 --><a href="#" class="fa fa-google-plus"></a><!--
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-6">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-5.jpg" data-at2x="pic/210x220-img-5@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>Jenny Doe</h3>
+											<span class="instructor-profession">Professor of Methematic</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!--
+												 --><a href="#" class="fa fa-facebook"></a><!--
+												 --><a href="#" class="fa fa-google-plus"></a><!--
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-1">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-1.jpg" data-at2x="pic/210x220-img-1@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>Jenny Doe</h3>
+											<span class="instructor-profession">Professor of Methematic</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!--
+												 --><a href="#" class="fa fa-facebook"></a><!--
+												 --><a href="#" class="fa fa-google-plus"></a><!--
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-2">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-2.jpg" data-at2x="pic/210x220-img-2@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>John Doe</h3>
+											<span class="instructor-profession">Lecturer of Design</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!--
+												 --><a href="#" class="fa fa-facebook"></a><!--
+												 --><a href="#" class="fa fa-google-plus"></a><!--
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- carousel people -->
+					<br>
+					<br>
+					<!-- carousel people -->
+					<div class="carousel-container">
+						<div class="title-carousel">
+							<h2>اساتید درس فلان</h2>
+							<div class="carousel-nav">
+								<div class="carousel-button">
+									<div class="prev"><i class="fa fa-angle-left"></i></div><!--
+								 --><div class="next"><i class="fa fa-angle-right"></i></div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-col-row left-margin-none">
+							<div class="owl-carousel owl-two-item">
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-2">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-2.jpg" data-at2x="pic/210x220-img-2@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>John Doe</h3>
+											<span class="instructor-profession">Lecturer of Design</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!--
+												 --><a href="#" class="fa fa-facebook"></a><!--
+												 --><a href="#" class="fa fa-google-plus"></a><!--
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-3">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-4.jpg" data-at2x="pic/210x220-img-4@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>Jessy Doe</h3>
+											<span class="instructor-profession">Professor of Methematic</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!--
+												 --><a href="#" class="fa fa-facebook"></a><!--
+												 --><a href="#" class="fa fa-google-plus"></a><!--
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-1">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-1.jpg" data-at2x="pic/210x220-img-1@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>Jenny Doe</h3>
+											<span class="instructor-profession">Professor of Methematic</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!--
+												 --><a href="#" class="fa fa-facebook"></a><!--
+												 --><a href="#" class="fa fa-google-plus"></a><!--
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="gallery-item">
+									<div class="item-instructor bg-color-6">
+										<a href="page-profile.html" class="instructor-avatar">
+											<img src="pic/210x220-img-5.jpg" data-at2x="pic/210x220-img-5@2x.jpg" alt>
+										</a>
+										<div class="info-box">
+											<h3>Jenny Doe</h3>
+											<span class="instructor-profession">Professor of Methematic</span>
+											<div class="divider"></div>
+											<p>Donec sollicitudin lacus in felis luctus blandit. Ut hendrerit mattis.</p>
+											<div class="social-link"><!--
+												 --><a href="#" class="fa fa-facebook"></a><!--
+												 --><a href="#" class="fa fa-google-plus"></a><!--
+												 --><a href="#" class="fa fa-twitter"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- carousel people -->
+				</div>
+			</section>
+		</main>
+	</div>
+<!-- footer -->
+@include('footer')
+<!-- / footer -->
+	<!-- scripts -->
+	<script src="js/jquery.min.js"></script>
+	<script type='text/javascript' src='js/jquery.validate.min.js'></script>
+	<script src="js/jquery.form.min.js"></script>
+	<script src="js/TweenMax.min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/select2.js"></script>
+	<script src="js/jquery.isotope.min.js"></script>
+	
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/jflickrfeed.min.js"></script>
+	<script src="js/jquery.tweet.js"></script>
+	<!--<script type='text/javascript' src='tuner/js/colorpicker.js'></script>-->
+	<!--<script type='text/javascript' src='tuner/js/scripts.js'></script>-->
+	<script src="js/jquery.fancybox.pack.js"></script>
+	<script src="js/jquery.fancybox-media.js"></script>
+	<script src="js/retina.min.js"></script>
+	<script src="js/Kimia.js"></script>
+	<!-- scripts -->
+</body>
+
+</html>
