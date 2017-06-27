c588e1925258efe65d0db9182cfe92953fbe4151
hossein <h.faghihi15@gmail.com>
2017-06-27 11:19:46 +0430

    reset page

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -3,7 +3,7 @@
 
 
 <head>
-    <title>UniLearn - Education and Courses Template</title>
+    <title>ریست کردن کلمه عبور</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     <!-- style -->
@@ -25,161 +25,87 @@
     @include('header')
     <!-- sticky -->
 </header>
-<main>
-    <section class="fullwidth-background bg-2">
-        <div class="grid-row">
-            <div class="login-block">
-                <div class="logo">
-                    <img src="/img/logo.png"  width="82" height="72" alt>
-                    <h2>وستا کمپ</h2>
+    <main>
+        <section class="fullwidth-background bg-2">
+            <div class="grid-row">
+                <div class="login-block">
+                    <div class="logo">
+                        <img src="/img/logo.png"  width="82" height="72" alt>
+                        <h2>وستا کمپ</h2>
+                    </div>
+
+                    <form class="login-form login-right-align" method="POST" action="{{ route('password.request') }}">
+                        {{ csrf_field() }}
+
+                        <input type="hidden" name="token" value="{{ $token }}">
+
+                        <label> : ایمیل خود را وارد کنید</label>
+                        <div class="form-group">
+                            <input type="email" class="login-input" placeholder="ایمیل" name="email" value="{{ $email or old('email') }}" required autofocus>
+                            <span class="input-icon">
+                                <i class="fa fa-lock"></i>
+                            </span>
+                        </div>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                        <label>رمز جدید را وارد کنید:</label>
+                        <div class="form-group">
+                            <input type="password" class="login-input" name="password" required placeholder=" کلمه عبور">
+                            <span class="input-icon">
+                                <i class="fa fa-lock"></i>
+                            </span>
+                        </div>
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                        <label>تکرار رمز را وارد کنید:</label>
+                        <div class="form-group">
+                            <input type="password" class="login-input" name="password_confirmation" required placeholder="تکرار کلمه عبور">
+                            <span class="input-icon">
+                                <i class="fa fa-lock"></i>
+                            </span>
+                        </div>
+                        @if ($errors->has('password_confirmtion'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password_confirmtion) }}</strong>
+                            </span>
+                        @endif
+
+                        <input type="submit" class="button-fullwidth cws-button bt-color-3 border-radius" value="ثبت">
+
+                    </form>
                 </div>
-
-                <form class="login-form login-right-align" method="POST" action="{{ url('/password/reset') }}">
-                    {{ csrf_field() }}
-
-                    <input type="hidden" name="token" value="{{ $token }}">
-
-                    <label> : کلمه عبور جدید خود را وارد کنید</label>
-                    <div class="form-group">
-                        <input type="email" class="login-input" placeholder="ایمیل" name="email" value="{{ $email or old('email') }}" required autofocus>
-                        <span class="input-icon">
-								<i class="fa fa-lock"></i>
-							</span>
-                    </div>
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                    @endif
-                    <div class="form-group">
-                        <input type="password" class="login-input" name="password" required placeholder=" کلمه عبور">
-                        <span class="input-icon">
-								<i class="fa fa-lock"></i>
-							</span>
-                    </div>
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                    @endif
-                    <div class="form-group">
-                        <input type="password" class="login-input" name="password_confirmation" required placeholder="تکرار کلمه عبور">
-                        <span class="input-icon">
-								<i class="fa fa-lock"></i>
-							</span>
-                    </div>
-                    @if ($errors->has('password_confirmtion'))
-                        <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmtion) }}</strong>
-                                    </span>
-                    @endif
-
-                    <a href="#" class="button-fullwidth cws-button bt-color-3 border-radius">ثبت </a>
-
-                </form>
             </div>
-        </div>
-    </section>
-</main>
-<!-- footer -->
-@include('footer')
-<!-- footer -->
-<!-- scripts -->
-<script src="js/jquery.min.js"></script>
-<script type='text/javascript' src='js/jquery.validate.min.js'></script>
-<script src="js/jquery.form.min.js"></script>
-<script src="js/TweenMax.min.js"></script>
-<script src="js/main.js"></script>
-<script src="js/select2.js"></script>
-<script src="js/jquery.isotope.min.js"></script>
+        </section>
+    </main>
+    <!-- footer -->
+    @include('footer')
+    <!-- footer -->
+    <!-- scripts -->
+    <script src="js/jquery.min.js"></script>
+    <script type='text/javascript' src='js/jquery.validate.min.js'></script>
+    <script src="js/jquery.form.min.js"></script>
+    <script src="js/TweenMax.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/select2.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
 
-<script src="js/owl.carousel-v2.js"></script>
-<script src="js/jquery-ui.min.js"></script>
-<script src="js/jflickrfeed.min.js"></script>
-<script src="js/jquery.tweet.js"></script>
-<!--<script type='text/javascript' src='tuner/js/colorpicker.js'></script>-->
-<!--<script type='text/javascript' src='tuner/js/scripts.js'></script>-->
-<script src="js/jquery.fancybox.pack.js"></script>
-<script src="js/jquery.fancybox-media.js"></script>
-<script src="js/retina.min.js"></script>
-<!-- scripts -->
+    <script src="js/owl.carousel-v2.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/jflickrfeed.min.js"></script>
+    <script src="js/jquery.tweet.js"></script>
+    <!--<script type='text/javascript' src='tuner/js/colorpicker.js'></script>-->
+    <!--<script type='text/javascript' src='tuner/js/scripts.js'></script>-->
+    <script src="js/jquery.fancybox.pack.js"></script>
+    <script src="js/jquery.fancybox-media.js"></script>
+    <script src="js/retina.min.js"></script>
+    <!-- scripts -->
 </body>
 
 
 </html>
-{{--@extends('layouts.app')--}}
-
-{{--@section('content')--}}
-{{--<div class="container">--}}
-    {{--<div class="row">--}}
-        {{--<div class="col-md-8 col-md-offset-2">--}}
-            {{--<div class="panel panel-default">--}}
-                {{--<div class="panel-heading">Reset Password</div>--}}
-
-                {{--<div class="panel-body">--}}
-                    {{--@if (session('status'))--}}
-                        {{--<div class="alert alert-success">--}}
-                            {{--{{ session('status') }}--}}
-                        {{--</div>--}}
-                    {{--@endif--}}
-
-                    {{--<form class="form-horizontal" role="form" method="POST" action="{{ route('password.request') }}">--}}
-                        {{--{{ csrf_field() }}--}}
-
-                        {{--<input type="hidden" name="token" value="{{ $token }}">--}}
-
-                        {{--<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">--}}
-                            {{--<label for="email" class="col-md-4 control-label">E-Mail Address</label>--}}
-
-                            {{--<div class="col-md-6">--}}
-                                {{--<input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>--}}
-
-                                {{--@if ($errors->has('email'))--}}
-                                    {{--<span class="help-block">--}}
-                                        {{--<strong>{{ $errors->first('email') }}</strong>--}}
-                                    {{--</span>--}}
-                                {{--@endif--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">--}}
-                            {{--<label for="password" class="col-md-4 control-label">Password</label>--}}
-
-                            {{--<div class="col-md-6">--}}
-                                {{--<input id="password" type="password" class="form-control" name="password" required>--}}
-
-                                {{--@if ($errors->has('password'))--}}
-                                    {{--<span class="help-block">--}}
-                                        {{--<strong>{{ $errors->first('password') }}</strong>--}}
-                                    {{--</span>--}}
-                                {{--@endif--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">--}}
-                            {{--<label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>--}}
-                            {{--<div class="col-md-6">--}}
-                                {{--<input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>--}}
-
-                                {{--@if ($errors->has('password_confirmation'))--}}
-                                    {{--<span class="help-block">--}}
-                                        {{--<strong>{{ $errors->first('password_confirmation') }}</strong>--}}
-                                    {{--</span>--}}
-                                {{--@endif--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<div class="form-group">--}}
-                            {{--<div class="col-md-6 col-md-offset-4">--}}
-                                {{--<button type="submit" class="btn btn-primary">--}}
-                                    {{--Reset Password--}}
-                                {{--</button>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</form>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</div>--}}
-{{--@endsection--}}
