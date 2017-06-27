2546c0ebded24762bb2dded275ab001073df6b75
hossein <h.faghihi15@gmail.com>
2017-06-27 11:10:16 +0430

    reset pass change

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'صحت اطلاعات شما تایید نشد',
+    'throttle' => 'تعداد زیادی درخواست داشته اید لحظاتی صبر کنید و مجدد اقدام نمایید',
 
 ];
