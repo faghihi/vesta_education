ec20a529ce14b6d558a75782a0c668e92ae926a2
Amin Mohammad <aminmohammadmazreali@gmail.com>
2017-06-28 16:51:35 +0430

    add QrFile Directory

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'vestacamp@vestaak.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'aminmohammadmazreali@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'وستاکمپ'),
     ],
 
