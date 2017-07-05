aa154a3350e5b3c2b2d5b834a08908a6b51bb810
hossein <h.faghihi15@gmail.com>
2017-07-05 19:05:30 +0430

    course controller change for pack

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -336,7 +336,7 @@ class CourseController extends Controller
     public function pack($id)
     {
         $course=Usecourse::findorfail($id);
-        $packages = $course->course->packages()->get();
+        $packages = $course->course->packages;
         return view('packages/packages-list')->with(['packs' => $packages]);
     }
 
