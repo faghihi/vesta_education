939fe45c263cc5f388d36a1c48ecbad24c8192d4
hossein <h.faghihi15@gmail.com>
2017-07-06 10:17:49 +0430

    404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>UniLearn - Education and Courses Template</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+	<!-- style -->
+	<link rel="shortcut icon" href="img/favicon.png">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/main.css">
+	<!--<link rel="stylesheet" type="text/css" href="tuner/css/colorpicker.css" />-->
+	<!--<link rel="stylesheet" type="text/css" href="tuner/css/styles.css" />-->
+	<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css" />
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/Kimia.css">
+	<!--styles -->
+</head>
+<body class="">
+	<header>
+		@include('header')
+		<div class="page-title">
+			<div class="grid-row">
+				<h1>صفحه 404</h1>
+				<nav class="bread-crumb">
+					<a href="index.html">خانه</a>
+					<i class="fa fa-long-arrow-left"></i>
+					<a href="content-elements.html">ویژگی ها</a>
+					<i class="fa fa-long-arrow-left"></i>
+					<a href="#">صفحه ها</a>
+					<i class="fa fa-long-arrow-left"></i>
+					<a href="#">صفحه 404</a>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<div class="page-content container clear-fix">
+		<div class="container-404">
+			<div class="number">4<span class="zero">0</span>4</div>
+			<p><span> متاسفیم :(</span><br/>این صفحه وجود ندارد.</p>
+			<a href="index.html" class="cws-button border-radius alt">به صفحه اصلی برو</a>
+		</div>
+		
+	</div>
+	@include('footer')
+	<script src="js/jquery.min.js"></script>
+	<script type='text/javascript' src='js/jquery.validate.min.js'></script>
+	<script src="js/jquery.form.min.js"></script>
+	<script src="js/TweenMax.min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/jquery.isotope.min.js"></script>
+	
+	<script src="js/owl.carousel-v2.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/jflickrfeed.min.js"></script>
+	<script src="js/jquery.tweet.js"></script>
+	<!--<script type='text/javascript' src='tuner/js/colorpicker.js'></script>-->
+	<!--<script type='text/javascript' src='tuner/js/scripts.js'></script>-->
+	<script src="js/jquery.fancybox.pack.js"></script>
+	<script src="js/jquery.fancybox-media.js"></script>
+	<script src="js/retina.min.js"></script>
+</body>
+
+</html>
